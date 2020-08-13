7c4debfa90973cea3cd723bb26747cacc7dc4db9
Backendmonk <59171380+Backendmonk@users.noreply.github.com>
2020-08-13 21:16:24 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,7 @@
                  $a = e(Auth::user()->level); 
         ?>
 @extends('layouts.conten')
-@section('ket')
-    @if($a == "admin")
-    Dashboard Admin
-                                      
-    @elseif($a == "user")
-    Dashboard User
-                                @endif
-@endsection
+
 @section('nama')
 <h6>{{Auth::user()->name}}<h6>
 @endsection
